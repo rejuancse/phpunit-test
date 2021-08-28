097e11aeafc4cb300fe1fdaa5a6a10a387f2387f
rejuancse <rejuancse@gmail.com>
2021-08-28 20:24:44 +0600

    Test Collection function check with Iterator Aggregate Implements

---
+++ tests/unit/CollectionTest.php
@@ -0,0 +1,44 @@
+<?php
+
+// namespace
+use PHPUnit\Framework\TestCase;
+
+class CollectionTest extends TestCase
+{
+    /** @test */
+    public function empty_instantiated_collection_returns_no_items()
+    {
+        $collection = new \App\Support\Collection;
+
+        $this->assertEmpty($collection->get());
+    }
+
+    //** @test */
+    public function count_is_correct_for_items_passed_in()
+    {
+        $collection = new \App\Support\Collection([
+            'one', 'two', 'three'
+        ]);
+    
+        $this->assertEquals(3, $collection->count());
+    }
+
+    //** @test */
+    public function items_returned_match_items_passed_in()
+    {
+        $collection = new \App\Support\Collection([
+            'one', 'two'
+        ]);
+    
+        $this->assertCount(2, $collection->get());
+        $this->assertEquals($collection->get()[0], 'one');
+        $this->assertEquals($collection->get()[1], 'two');
+    }
+
+    //** @test */
+    public function collection_is_instance_of_iterator_aggregate()
+    {
+        $collection = new \App\Support\Collection();
+        $this->assertInstanceOf(IteratorAggregate::class, $collection);
+    }
+}
